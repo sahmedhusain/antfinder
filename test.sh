b0013d9040efb918f7d83a12206556902e81ebb9
Sayed Ahmed Husain <160347327+sahmedhusain@users.noreply.github.com>
2024-10-28 18:29:22 +0300

    edit test file

---
+++ test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+clear
+echo -e "v--------------example00.txt--------------v\n"
+go run main.go examples/example00.txt
+echo -e "\nv--------------example01.txt--------------v\n"
+go run main.go examples/example01.txt
+echo -e "\nv--------------example02.txt--------------v\n"
+go run main.go examples/example02.txt
+echo -e "\nv--------------example03.txt--------------v\n"
+go run main.go examples/example03.txt
+echo -e "\nv--------------example04.txt--------------v\n"
+go run main.go examples/example04.txt
+echo -e "\nv-------------example05.txt-------------v\n"
+go run main.go examples/example05.txt
+echo -e "\nv-------------example06.txt-------------v\n"
+go run main.go examples/example06.txt
+echo -e "\nv-------------example07.txt-------------v\n"
+go run main.go examples/example07.txt
+echo -e "\nv-----------badexample00.txt------------v\n"
+go run main.go examples/badexample00.txt
+echo -e "\nv-----------badexample01.txt------------v\n"
+go run main.go examples/badexample01.txt
