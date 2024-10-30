3033391ad5e949f05364610f3c145f285fdb4ce7
Sayed Ahmed Husain <160347327+sahmedhusain@users.noreply.github.com>
2024-10-31 00:38:42 +0300

    fixing readfile and state return

--- main.go
+++ main.go
@@ -2,27 +2,13 @@ package main
 
 import (
 	"fmt"
-	structs "lem-in/datastruct"
 	move "lem-in/logic/antsmoving"
-	paths "lem-in/logic/paths"
-	rooms "lem-in/logic/rooms"
 	fileoperations "lem-in/operations/file"
 	stringoperations "lem-in/operations/strings"
 	"os"
 )
 
 func main() {
-	state := &structs.State{
-		Visited:         make(map[string]bool),
-		Tunnels:         make(map[string][]string),
-		RoomsMapRoom:    make(map[string]bool),
-		RoomsMapTunnels: make(map[string]string),
-		StartEnd:        make(map[string]int),
-		StartEndRooms:   make(map[string]string),
-		RoomCoordinates: make(map[string]int),
-		Solution:        make(map[string]string),
-		Connect:         make(map[string]int),
-	}
 	if len(os.Args) > 2 {
 		fmt.Println("Error: too many arguments")
 		fmt.Println("Usage: go run main.go <filename>")
@@ -31,55 +17,22 @@ func main() {
 		fmt.Println("Usage: go run main.go <filename>")
 		os.Exit(1)
 	} else if len(os.Args) == 2 {
-		fileoperations.ReadFile(os.Args[1], state)
+		state := fileoperations.ReadFile(os.Args[1])
 
-		if !rooms.Roomtunels(state.RoomsMapTunnels, state.Rooms) {
-			fmt.Println("Error: tunnels do not connect to a valid room")
-			os.Exit(1)
-		}
-		if state.RoomsMapTunnels[state.End] == "" {
-			fmt.Println("Error: start and end rooms do not connect")
-			os.Exit(1)
-		}
-		if state.StartEndRooms["start"] != "" && state.StartEndRooms["end"] != "" && len(state.StartAndEnd) == 2 {
-			startedroom := state.StartEndRooms["start"]
-			endedroom := state.StartEndRooms["end"]
-			for _, route := range state.Tunnels[startedroom] {
-				path := []string{}
-				state.Visited[startedroom] = true
-				paths.Start(path, endedroom, route, state)
-			}
-			if state.Ants == 0 {
-				fmt.Println("error, no Ants found")
-				return
-			} else if state.Ants > 1000 {
-				fmt.Println("error, Ants number is too large, Max Ants number is 1000")
-				return
-			}
-			state.Paths = stringoperations.SortByLength(stringoperations.GetUniqueStringSets(state.Paths))
-			solution := stringoperations.EqualizeSlices(move.MoveLogic(state.Ants, state.Paths, state.End))
+		solution := stringoperations.EqualizeSlices(move.MoveLogic(state.Ants, state.Paths, state.End))
 
-			if state.Paths == nil {
-				fmt.Println("error, no paths connect the start to the end")
-				return
-			} else {
-				for i := 0; i < len(solution[0]); i++ {
-					for j := 0; j < state.Ants; j++ {
-						if i >= len(solution[j]) || solution[j][i] == "wait" {
-							continue
-						}
-						fmt.Print("L")
-						fmt.Print(j + 1)
-						fmt.Print("-")
-						fmt.Print(solution[j][i])
-						fmt.Print(" ")
-					}
-					fmt.Println()
+		for i := 0; i < len(solution[0]); i++ {
+			for j := 0; j < state.Ants; j++ {
+				if i >= len(solution[j]) || solution[j][i] == "wait" {
+					continue
 				}
+				fmt.Print("L")
+				fmt.Print(j + 1)
+				fmt.Print("-")
+				fmt.Print(solution[j][i])
+				fmt.Print(" ")
 			}
+			fmt.Println()
 		}
-	} else {
-		fmt.Println("Error: start and end rooms are not defined or are duplicated")
-		os.Exit(1)
 	}
 }
--- operations/file/readfile.go
+++ operations/file/readfile.go
@@ -4,16 +4,28 @@ import (
 	"bufio"
 	"fmt"
 	structs "lem-in/datastruct"
+	paths "lem-in/logic/paths"
+	rooms "lem-in/logic/rooms"
 	stringoperations "lem-in/operations/strings"
 	"os"
 )
 
-func ReadFile(FilePath string, state *structs.State) {
-
+func ReadFile(FilePath string) *structs.State {
+	state := &structs.State{
+		Visited:         make(map[string]bool),
+		Tunnels:         make(map[string][]string),
+		RoomsMapRoom:    make(map[string]bool),
+		RoomsMapTunnels: make(map[string]string),
+		StartEnd:        make(map[string]int),
+		StartEndRooms:   make(map[string]string),
+		RoomCoordinates: make(map[string]int),
+		Solution:        make(map[string]string),
+		Connect:         make(map[string]int),
+	}
 	f, err := os.Open(FilePath)
 	if err != nil {
 		fmt.Println("Error opening file:", err)
-		return
+		os.Exit(1)
 	}
 	defer f.Close()
 	scanner := bufio.NewScanner(f)
@@ -21,4 +33,40 @@ func ReadFile(FilePath string, state *structs.State) {
 		line := scanner.Text()
 		stringoperations.VarState(line, state)
 	}
+
+	if !rooms.Roomtunels(state.RoomsMapTunnels, state.Rooms) {
+		fmt.Println("Error: tunnels do not connect to a valid room")
+		os.Exit(1)
+	}
+	if state.RoomsMapTunnels[state.End] == "" {
+		fmt.Println("Error: start and end rooms do not connect")
+		os.Exit(1)
+	}
+	if state.StartEndRooms["start"] != "" && state.StartEndRooms["end"] != "" && len(state.StartAndEnd) == 2 {
+		startedroom := state.StartEndRooms["start"]
+		endedroom := state.StartEndRooms["end"]
+		for _, route := range state.Tunnels[startedroom] {
+			path := []string{}
+			state.Visited[startedroom] = true
+			paths.Start(path, endedroom, route, state)
+		}
+		if state.Ants == 0 {
+			fmt.Println("error, no Ants found")
+			os.Exit(1)
+		} else if state.Ants > 1000 {
+			fmt.Println("error, Ants number is too large, Max Ants number is 1000")
+			os.Exit(1)
+		}
+		state.Paths = stringoperations.SortByLength(stringoperations.GetUniqueStringSets(state.Paths))
+
+		if state.Paths == nil {
+			fmt.Println("error, no paths connect the start to the end")
+			os.Exit(1)
+		} else {
+			return state
+		}
+	}
+	fmt.Println("Error: start and end rooms are not defined or are duplicated")
+	os.Exit(1)
+	return nil
 }
